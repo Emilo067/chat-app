7045ef2c285ccab97dc3d6882864f93cc05d16a5
Emilo067 <gasanovemil467@gmail.com>
2024-06-13 21:49:19 +0300

    Remove HeaderContainer.tsx and add in Header.tsx dispatch and useSelectors

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,54 +1,34 @@
 import React from 'react';
-import styled from "styled-components";
 import {NavLink} from "react-router-dom";
+import {S} from "./Header.styles"
+import {useAppSelector} from "../../common/hooks/useAppSelector";
+import {selectIsAuth, selectLogin} from "../../redux/auth-selectors";
+import {logout} from "../../redux/auth-reducer";
+import {useAppDispatch} from "../../common/hooks/useAppDispatch";
 
-type HeaderPropsType = {
-    login: string | null
-    isAuth: boolean
-    logout: () => void
-}
+const Header = () => {
 
-const Header = ({login, isAuth, logout}: HeaderPropsType) => {
+    const login = useAppSelector<string | null>(selectLogin)
+    const isAuth = useAppSelector<boolean>(selectIsAuth)
+    const dispatch = useAppDispatch()
 
-    const onCLickHandler = () => {
-        logout()
+    const logoutHandler = () => {
+        dispatch(logout())
     }
 
     return (
-        <StyledHeader>
+        <S.StyledHeader>
             <img alt={""} src={"https://png.pngtree.com/template/20191024/ourmid/pngtree-mountain-landscape-logo-design-hiking-travel-and-adventure-concept-design-image_323034.jpg"}/>
 
             <div>
                 {login ? <span>{login}</span> : <NavLink to={'/login'}>Login</NavLink>}
-                {isAuth ? <button onClick={onCLickHandler}>logout</button> : "" }
+                {isAuth ? <button onClick={logoutHandler}>logout</button> : "" }
             </div>
 
-        </StyledHeader>
+        </S.StyledHeader>
     );
 };
 
 export default Header;
 
 
-const StyledHeader = styled.header`
-  grid-area: h;
-  background-color: orange;
-  
-  div {
-    display: flex;
-    justify-content: right;
-    gap: 10px;
-  }
-  
-  img {
-    width: 20px;
-  }
-  
-  a {
-    float: right;
-  }
-
-  span {
-    float: right;
-  }
-`
