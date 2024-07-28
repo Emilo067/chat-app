0c80b9ac568b83cd672f630d11d8809c45a8a644
Emilo067 <gasanovemil467@gmail.com>
2024-07-28 21:40:00 +0300

    change import

--- src/common/hooks/useAppSelector.ts
+++ src/common/hooks/useAppSelector.ts
@@ -1,4 +1,4 @@
 import {TypedUseSelectorHook, useSelector} from "react-redux";
-import {AppStateType} from "../../redux/store-redux";
+import {AppStateType} from "../../app/store/store-redux";
 
 export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector
