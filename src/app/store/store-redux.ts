69aadc0867496c9cfce45191656f21e215a6b7d6
Emilo067 <gasanovemil467@gmail.com>
2024-07-28 21:40:21 +0300

    move

--- src/app/store/store-redux.ts
+++ src/app/store/store-redux.ts
@@ -1,13 +1,13 @@
 import {AnyAction, applyMiddleware, combineReducers, createStore} from "redux";
-import {sidebarReducer} from "./sidebar-reducer";
-import {profileReducer} from "../layout/content/profile/model/profile-reducer";
-import {dialogsReducer} from "./dialogs-reducer";
-import {usersReducer} from "./users-reducer";
-import {authReducer} from "./auth-reducer";
+import {sidebarReducer} from "../../redux/sidebar-reducer";
+import {profileReducer} from "../../layout/content/profile/model/profile-reducer";
+import {dialogsReducer} from "../../redux/dialogs-reducer";
+import {usersReducer} from "../../layout/content/Users/model/reducer/users-reducer";
+import {authReducer} from "../../redux/auth-reducer";
 import thunk, {ThunkDispatch} from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as formReducer } from 'redux-form'
-import {appReducer} from "./app-reducer";
+import {appReducer} from "../../redux/app-reducer";
 
 
 let rootReducer = combineReducers({
