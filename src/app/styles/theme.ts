32a8ebb544fb2291ae6c5508589611e0349ce82b
Emilo067 <gasanovemil467@gmail.com>
2024-07-28 21:40:14 +0300

    create theme

---
+++ src/app/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+    colors: {
+        primaryBg: 'rgba(42,107,4,0.9)',
+        secondaryBg: 'rgba(5,54,145,0.9)',
+
+        border: '1px solid rgba(91,222,18, 0.9)',
+
+        accent: '#00BCD4',
+
+        font: '#ffffff',
+
+        gradient: 'linear-gradient(270deg, #13ADC7 0%, #6978D1 66.67%, #945DD6 100%)',
+        gradientHover: 'linear-gradient(135deg, #F36265 0%, #FFA500 100%)'
+    },
+
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)"
+    }
+}
