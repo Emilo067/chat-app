e6fd1584c4ffdee6c7227060ec815f278fa0ef60
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 12:13:22 +0300

    change import

--- src/redux/auth-selectors.ts
+++ src/redux/auth-selectors.ts
@@ -1,4 +1,4 @@
-import {AppStateType} from "./store-redux";
+import {AppStateType} from "../app/store/store-redux";
 
 export const selectIsAuth = (state: AppStateType) => state.auth.isAuth
 export const selectLogin = (state: AppStateType) => state.auth.login
