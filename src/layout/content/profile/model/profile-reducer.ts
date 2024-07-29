7ba5337b11d1f808aa4f2360a8961b5bc15fdc8a
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 12:02:39 +0300

    changes

--- src/layout/content/profile/model/profile-reducer.ts
+++ src/layout/content/profile/model/profile-reducer.ts
@@ -1,38 +1,47 @@
 import {profileApi, usersApi} from "../../../../api/api";
 import {Dispatch} from "redux";
 import {ResultCode} from "../../../../common/enums/enums";
+import {FormProfileData} from "../ui/profile/profileSetDataForm/ProfileSetDataForm";
+import {AppStateType} from "../../../../app/store/store-redux";
+import {stopSubmit} from "redux-form";
 
 const ADD_POST = 'ADD-POST'
 const SET_USER_PROFILE = 'SET-USER-PROFILE'
 const SET_PROFILE_STATUS = 'SET-PROFILE-STATUS'
 const SET_PROFILE_PHOTO = 'SET-PROFILE-PHOTO'
+const SET_PROFILE_UPDATE_STATUS = 'SET-PROFILE-UPDATE-STATUS'
 
 type ActionType =
     AddPostACType
     | SetUserProfileACType
     | SetProfileStatusACType
     | SetProfilePhotoACType
+    | SetProfileUpdateStatusACType
 
 
-export type PostType = {
+export type Post = {
     post: string,
     likes: number
 }
 
+export type ProfileUpdateStatus = 'none' | 'success' | 'error'
+
+export type ContactsType = {
+    "facebook": string,
+    "website": string,
+    "vk": string,
+    "twitter": string,
+    "instagram": string,
+    "youtube": string,
+    "github": string,
+    "mainLink": string
+}
+
 export type ProfileType = {
-    aboutMe: string ,
-    contacts: {
-        facebook:  string,
-        website:  string,
-        vk:  string,
-        twitter:  string,
-        instagram:  string,
-        youtube:  string,
-        github:  string,
-        mainLink:  string
-    },
+    aboutMe: string,
+    contacts: ContactsType,
     lookingForAJob: boolean,
-    lookingForAJobDescription:  string,
+    lookingForAJobDescription: string,
     fullName: string,
     userId: number,
     photos: {
@@ -42,9 +51,10 @@ export type ProfileType = {
 } | null
 
 export type ProfilePageType = {
-    posts: PostType[]
+    posts: Post[]
     profile: ProfileType
     status: string
+    profileUpdateStatus: ProfileUpdateStatus
 }
 
 // export type InitialStateType = {
@@ -58,6 +68,7 @@ const initialState: ProfilePageType = {
         {post: "This is my post", likes: 33},
         {post: "This is my post", likes: 4}
     ],
+    profileUpdateStatus: 'none',
     profile: null,
     status: ''
 }
@@ -81,14 +92,18 @@ export const profileReducer = (state: ProfilePageType = initialState, action: Ac
                 status: action.status
             }
         case SET_PROFILE_PHOTO:
-            debugger
             if (state.profile) {
                 return {
                     ...state,
-                    profile: { ...state.profile, photos: action.photos }
+                    profile: {...state.profile, photos: action.photos}
                 };
             }
-            return state;
+            return state
+        case SET_PROFILE_UPDATE_STATUS:
+            return {
+                ...state,
+                profileUpdateStatus: action.status
+            }
         default:
             return state
     }
@@ -98,6 +113,7 @@ export type AddPostACType = ReturnType<typeof addPostAC>
 export type SetUserProfileACType = ReturnType<typeof setUserProfileAC>
 export type SetProfileStatusACType = ReturnType<typeof setProfileStatus>
 export type SetProfilePhotoACType = ReturnType<typeof setProfilePhoto>
+export type SetProfileUpdateStatusACType = ReturnType<typeof setProfileUpdateStatus>
 
 export const addPostAC = (newPost: string) => {
     return {type: ADD_POST, newPost} as const
@@ -109,6 +125,10 @@ export const setUserProfileAC = (profile: ProfileType) => {
 
 export const setProfileStatus = (status: string) => ({type: SET_PROFILE_STATUS, status} as const)
 export const setProfilePhoto = (photos: any) => ({type: SET_PROFILE_PHOTO, photos} as const)
+export const setProfileUpdateStatus = (status: ProfileUpdateStatus) => ({
+    type: SET_PROFILE_UPDATE_STATUS,
+    status
+} as const)
 
 
 export const getUserProfile = (userId: number) => (dispatch: Dispatch) => {
@@ -141,6 +161,23 @@ export const updatePhoto = (file: any) => (dispatch: Dispatch) => {
             dispatch(setProfilePhoto(res.data.data.photos))
         } else {
             console.warn("ERROR UPDATE PHOTO")
+        }
+    })
+}
+
+export const updateProfile = (profile: FormProfileData) => (dispatch: Dispatch<any>, getState: () => AppStateType) => {
+    const userId = getState().auth.id
+    profileApi.updateProfile(profile).then(res => {
+        if (res.data.resultCode === ResultCode.success) {
+            dispatch(setProfileUpdateStatus('success'))
+            if (userId) {
+                dispatch(getUserProfile(+userId))
+            }
+        } else {
+            let message = res.data.messages.length > 0 ? res.data.messages[0] : "Some error"
+            dispatch(setProfileUpdateStatus('error'))
+            dispatch(stopSubmit('edit-profile', {_error: message}))
+            //dispatch(stopSubmit('edit-profile', {'contacts': {'facebook': message}}))
         }
     })
 }
